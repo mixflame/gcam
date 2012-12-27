73e0b2365774eb601e506a4c5cab8229de982cfd
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-12-26 17:29:16 -0800

    get it booting

--- gcam-ios/app/app_delegate.rb
+++ gcam-ios/app/app_delegate.rb
@@ -1,5 +1,32 @@
 class AppDelegate
+  attr_accessor :window
+
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    $queue = Dispatch::Queue.new('com.jonsoft.globalchat')
+
+    UIApplication.sharedApplication.setStatusBarHidden(true, animated:false)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+
+    # switch_to_vc(load_vc("ServerList"))
     true
   end
+
+  def load_vc(identifier)
+    if Device.iphone?
+      storyboard = UIStoryboard.storyboardWithName("GC2-ios", bundle: NSBundle.mainBundle)
+    else
+      storyboard = UIStoryboard.storyboardWithName("GC2-ios-ipad", bundle: NSBundle.mainBundle)
+    end
+    vc = storyboard.instantiateViewControllerWithIdentifier(identifier)
+  end
+
+  def switch_to_vc(vc)
+    unless @window.rootViewController == vc
+      @window.rootViewController = vc
+      @window.rootViewController.wantsFullScreenLayout = true
+      @window.makeKeyAndVisible
+    end
+  end
+
 end
